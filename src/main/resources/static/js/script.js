310e5ec3cc5b1d9caf71a301ad3f56bb4fb34efc
graminvikaspahal <vinodsharma740860@gmail.com>
2024-09-25 17:06:58 +0530

    sidebar fixed

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,11 @@
 function openNav() {
-    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("sidebar").classList.add('sidebarOpen');
+    document.getElementById("sidebar").classList.add('');
     document.querySelector(".main-content").style.marginLeft = "250px";
 }
 
 function closeNav() {
     document.getElementById("sidebar").style.width = "0";
+    document.getElementById("sidebar").classList.remove('sidebarOpen');
     document.querySelector(".main-content").style.marginLeft = "0";
 }
